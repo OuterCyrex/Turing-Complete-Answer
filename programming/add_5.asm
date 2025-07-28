20cd1be6232b3e97b876df040858e30b580a8511
OuterCyrex <a15839962056@outlook.com>
2025-07-28 13:39:44 +0800

    🎉 feat: init the project

---
+++ programming/add_5.asm
@@ -0,0 +1,14 @@
+# copy input to reg 1
+10110001 (177)
+
+# copy number 5 to reg 0
+00000101 (5)
+
+# copy reg 0 to reg 2
+10000010 (130)
+
+# add
+01000100 (68)
+
+# copy reg 3 to output
+10011110 (158)
